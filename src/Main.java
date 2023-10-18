55515ebcd20a309610d9c558e6427ed465874aa6
Geeks <kennyyabe@gmail.com>
2023-10-18 12:26:18 -0500

    Ultimo cambio

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner cargaDeDatos = new Scanner(System.in);
-        System.out.println("Ingresa tu peso pe mascota ");
-        double Peso = cargaDeDatos.nextDouble();
-        System.out.println("Ingresa tu altura pe mmahuevo ");
-        double Altura = cargaDeDatos.nextDouble();
-        double IndiceMasaCorporal = Peso / (Altura * Altura);
-        System.out.println("Tu Indice masa Corporal es: " + IndiceMasaCorporal);
-        if (IndiceMasaCorporal >= 30.0) {
-            System.out.println("Estas OBESO deja de tragar brother");
-        }
 
-        if (IndiceMasaCorporal >= 25.0 && IndiceMasaCorporal <= 30.0) {
-            System.out.println("Estas con sobre peso oe gord@");
-        }
-
-        if (IndiceMasaCorporal >= 18.5 && IndiceMasaCorporal <= 25.0) {
-            System.out.println("Estas normal sigue con los mostritos nmas bro");
-        }
-
-        if (IndiceMasaCorporal <= 18.6) {
-            System.out.println("Estas flaco oe metele mas alitas mix");
-        }
+        //Calculadora de edad de un perro
+        Scanner Teclado = new Scanner(System.in);
+        Integer EdadPerro;
+        Integer EdadTotalPerro;
+        System.out.println("Ingrese la Edad de su Firulais");
+        EdadPerro = Teclado.nextInt();
+        EdadTotalPerro= EdadPerro * 7 ;
+        System.out.println("Esta es la edad de tu Tapir :" + EdadTotalPerro);
 
     }
 }
