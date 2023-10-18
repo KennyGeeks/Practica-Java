be771cddd377014a945cd05af4a920f6316f93bd
Geeks <kennyyabe@gmail.com>
2023-10-18 14:18:16 -0500

    2do cambio

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner Teclado = new Scanner(System.in);
+        Integer Precio;
+        Integer Porcentaje;
+        Integer Descuento;
+        System.out.println("Ingresa tu Precio sin descuento");
+        System.out.println("Ingresa tu Porcentaje de descuento pex ");
+        Descuento = (Precio * Porcentaje) / 100 ;
+        System.out.println("Este Seria tu precio con Descuento pe Misio: " + Descuento);
+    }
+    }
 
-        //Calculadora de edad de un perro
+
+        /* TAREA2: Calculadora de Millas A Kilometros
+        Scanner Teclado = new Scanner(System.in);
+        double Millas;
+        double Kilometros;
+        System.out.println("Ingresa las Millas pe JalaBolas");
+        Millas = Teclado.nextDouble();
+        Kilometros = Millas *1.60934;
+        System.out.println("Este es tu kilometraje de tus millas pe mascota :" + Kilometros);
+
+    } */
+
+
+        /* TAREA1: Calculadora de edad de un perro
         Scanner Teclado = new Scanner(System.in);
         Integer EdadPerro;
         Integer EdadTotalPerro;
@@ -14,5 +37,5 @@ public class Main {
         EdadTotalPerro= EdadPerro * 7 ;
         System.out.println("Esta es la edad de tu Tapir :" + EdadTotalPerro);
 
-    }
-}
+    } */
+
