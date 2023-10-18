ed4adc0c12a7a3e40350916e4a8d2a3827a35814
Geeks <kennyyabe@gmail.com>
2023-10-18 11:30:59 -0500

    primer cambio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner cargaDeDatos = new Scanner(System.in);
         System.out.println("Ingresa tu peso pe mascota ");
