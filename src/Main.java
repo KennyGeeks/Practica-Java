091e184378fa7abfdc57e95e15b518fa5eae71e6
Geeks <kennyyabe@gmail.com>
2023-10-26 13:02:26 -0500

    Ultimo Cambio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
 import java.util.Scanner;
-public class Main{
+public class Main {
     public static void main(String[] args) {
+
+
+    }
+}
+
+/* TAREA 7 PEDIR AL USUARIO QUE INGRESE SU PAIS E IMPRIMIR EL PAIS QUE INGRESO CON LINK DE GOOGLE
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese Su Pais : ");
+        String Pais = teclado.nextLine();
+        System.out.println("https://www.google.com/search?q=" + Pais);
+
+         */
+
+/* TAREA 6 CALCULAR NUMEROS PARES E IMPARES
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese su Primer Valor: ");
+        Integer Numero1 = teclado.nextInt();
+        Integer resto = Numero1 % 2;
+        if (resto == 0 ) {
+            System.out.println("Es Numero Par ");
+        } else {
+            System.out.println("Es Numero IMPAR");
+        }
+        */
+
+/*      TAREA 5 DIGITAR 3 VALORES E INDICAR CUAL ES EL MAYOR DE LOS 3
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese su Primer Valor: ");
+        Integer numero1 = teclado.nextInt();
+        System.out.println("Ingrese su Segundo Valor: ");
+        Integer numero2 = teclado.nextInt();
+        System.out.println("Ingrese su Tercer Valor: ");
+        Integer numero3 = teclado.nextInt();
+
+        if (numero1 > numero2 && numero1 > numero3) {
+            System.out.println(" El Valor mayor es: " + numero1);
+        }
+        if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println(" El Valor mayor es: " + numero2);
+        }
+        if (numero3 > numero2 && numero3 > numero1) {
+            System.out.println(" El Valor mayor es: " + numero3);
+        }
+        */
+
+/*  TAREA 4 ADIVINAR UN NUMERO ALEATORIO
+
     Scanner teclado = new Scanner(System.in);
     Integer NumeroUsuario = 0;
     Integer NumeroAleatorio = (int) Math.floor(Math.random() * 100);
@@ -12,9 +61,7 @@ if (NumeroUsuario == NumeroAleatorio) {
     System.out.println("Numero Equivocado ");
 }
     }
-    }
-
-
+    */
 
 /*  TAREA 3 : CALCULADORA DE PRECIOS CON DESCUENTO APLICADO
 
@@ -28,9 +75,10 @@ if (NumeroUsuario == NumeroAleatorio) {
         Porcentaje = Teclado.nextInt();
         PrecioTotal = PrecioRegular - (PrecioRegular * Porcentaje) / 100;
         System.out.println("Precio Total con Descuento Aplicado: " + PrecioTotal);
-    }*/
+    }
+    */
 
-        /* TAREA2: Calculadora de Millas A Kilometros
+/* TAREA2: Calculadora de Millas A Kilometros
 
         Scanner Teclado = new Scanner(System.in);
         double Millas;
@@ -39,10 +87,10 @@ if (NumeroUsuario == NumeroAleatorio) {
         Millas = Teclado.nextDouble();
         Kilometros = Millas *1.60934;
         System.out.println("Este es tu kilometraje de tus millas:" + Kilometros);
+    }
+    */
 
-    } */
-
-        /* TAREA1: Calculadora de edad de un perro
+/* TAREA1: Calculadora de edad de un perro
 
         Scanner Teclado = new Scanner(System.in);
         Integer EdadPerro;
@@ -52,5 +100,5 @@ if (NumeroUsuario == NumeroAleatorio) {
         EdadTotalPerro= EdadPerro * 7 ;
         System.out.println("Esta es la edad de tu Tapir :" + EdadTotalPerro);
 
-    } */
-
+    }
+    */
